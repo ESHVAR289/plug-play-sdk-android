312993b23ca964361aff6782241c4706c553d170
vijay <vijay.mandge@citruspay.com>
2016-12-28 13:13:16 +0530

    * Removed Outdated library

--- citruspay-sdk-ui/src/main/java/com/citrus/sdk/ui/events/CardSavedEvent.java
+++
@@ -1,7 +0,0 @@
-package com.citrus.sdk.ui.events;
-
-/**
- * Created by akshaykoul on 6/5/15.
- */
-public class CardSavedEvent {
-}
--- citruspay-sdk-ui/src/main/java/com/citrus/sdk/ui/utils/AnimationType.java
+++
@@ -1,8 +0,0 @@
-package com.citrus.sdk.ui.utils;
-
-/**
- * Created by akshaykoul on 6/2/15.
- */
-public enum AnimationType {
-    ANIM_PLAY,ANIM_REVERSE
-}
